40b9b10fe37316a4a2fbf02fe5126b68d5738a0a
clarencelol <clarencekuiek@proton.me>
2024-12-09 10:42:03 +0800

    Revert "Add IProducerListener::onBufferAttached"
    This reverts commit 6d223cbd07f0582ac99e4b607f6983284638aa0b.
    
    Signed-off-by: iHSAN <ihsanulrahman@gmail.com>

--- libs/gui/BufferQueueProducer.cpp
+++ libs/gui/BufferQueueProducer.cpp
@@ -1368,9 +1368,6 @@ status_t BufferQueueProducer::connect(const sp<IProducerListener>& listener,
 #endif
                 mCore->mConnectedProducerListener = listener;
                 mCore->mBufferReleasedCbEnabled = listener->needsReleaseNotify();
-#if COM_ANDROID_GRAPHICS_LIBGUI_FLAGS(BQ_CONSUMER_ATTACH_CALLBACK)
-                mCore->mBufferAttachedCbEnabled = listener->needsAttachNotify();
-#endif
             }
             break;
         default:
