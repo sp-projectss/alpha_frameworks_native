c3054d95e5d661037dce588b1b5af2edf66c2a46
clarencelol <clarencekuiek@proton.me>
2024-12-09 10:41:55 +0800

    Revert "Add missing IProducerListener callbacks to BLASTBufferQueue"
    This reverts commit 7c935090e839ec3391c6f94d46a2367e59a46056.
    
    Signed-off-by: iHSAN <ihsanulrahman@gmail.com>

--- libs/gui/BLASTBufferQueue.cpp
+++ libs/gui/BLASTBufferQueue.cpp
@@ -1124,17 +1124,6 @@ public:
         AsyncWorker::getInstance().post(
                 [listener = mListener, slots = slots]() { listener->onBuffersDiscarded(slots); });
     }
-
-    void onBufferDetached(int slot) override {
-        AsyncWorker::getInstance().post(
-                [listener = mListener, slot = slot]() { listener->onBufferDetached(slot); });
-    }
-
-#if COM_ANDROID_GRAPHICS_LIBGUI_FLAGS(BQ_CONSUMER_ATTACH_CALLBACK)
-    void onBufferAttached() override {
-        AsyncWorker::getInstance().post([listener = mListener]() { listener->onBufferAttached(); });
-    }
-#endif
 };
 
 // Extends the BufferQueueProducer to create a wrapper around the listener so the listener calls
